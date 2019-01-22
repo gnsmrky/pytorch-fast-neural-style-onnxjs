4a0b8d1d9c5760ddfa03b008cf2a9a5840ce4938
gnsmrky <geniusmark@gmail.com>
2019-01-22 23:32:10 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Run PyTorch exported .onnx model from [Fast-Neural-Style](https://github.com/pytorch/examples/tree/master/fast_neural_style)
+
+Loosely based on 
+[ONNX.js add example](https://github.com/Microsoft/onnxjs/tree/master/examples/browser/add).
+
+Install npm:
+```
+npm install http-server -g
+```
+
+To run the example:
+```
+http-server . -c-1 -p 3000
+```
+
+Open a browser and go to the following URL:
+```
+http://localhost:3000
+```
+
---
+++ index.js
@@ -0,0 +1,168 @@
+
+
+function setCanvasRGB(canvasId, r, g, b, a=0xFF){
+  var ctx = document.getElementById(canvasId).getContext("2d");
+  var h = ctx.canvas.height;
+  var w = ctx.canvas.width;
+
+  var imgData = ctx.getImageData(0, 0, w, h);
+  var data = imgData.data;
+
+  i=0;
+  for (var y=0; y<h; y++) {
+    for (var x=0; x<w; x++) {
+      data[i++]=r;
+      data[i++]=g;
+      data[i++]=b;
+      data[i++]=a;
+    }
+  }
+
+  ctx.putImageData(imgData, 0, 0);
+
+}
+
+var imgUrl_128 = "./images/amber_128x128.jpg";
+var canvas_128_src = "canvas_128_src";
+var canvas_128_dst = "canvas_128_dst";
+
+var imgUrl_256 = "./images/amber_256x256.jpg";
+var canvas_256_src = "canvas_256_src";
+var canvas_256_dst = "canvas_256_dst";
+
+window.onload = function() {
+  loadImage(imgUrl_128, canvas_128_src);
+  loadImage(imgUrl_256, canvas_256_src);
+}
+
+//
+// loadImage()
+//     imageUrl - URL for image to load
+//     canvasId - target canvas ID
+//     completeProc - callback when complete
+//
+function loadImage (imageUrl, canvasId, completeProc=null) {
+  var img = new Image();
+  img.src = imageUrl
+  img.onload=function() {
+    var ctx = document.getElementById(canvasId).getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    if (completeProc != null) {
+      completeProc();
+    }
+  }
+}
+
+function contextToTensor (canvasId) {
+  var ctx = document.getElementById(canvasId).getContext("2d");
+
+  const n = 1
+  const c = 3
+  const h = ctx.canvas.height
+  const w = ctx.canvas.width
+  
+  // load src context to a tensor
+  var srcImgData = ctx.getImageData(0, 0, w, h);
+  var src_data = srcImgData.data;
+  
+  const out_data   = new Float32Array(n*c*h*w);
+  
+  var src_idx = 0;
+  var out_idx_r = 0;
+  var out_idx_g = out_idx_r + h*w;
+  var out_idx_b = out_idx_g + h*w;
+
+  for (var y=0; y<h; y++) {
+    for (var x=0; x<w; x++) {
+      src_r = src_data[src_idx++];
+      src_g = src_data[src_idx++];
+      src_b = src_data[src_idx++];
+      src_idx++;
+
+      out_data[out_idx_r++] = src_r;// / 255.0;// * 255.0;
+      out_data[out_idx_g++] = src_g;// / 255.0;//128.0 * 255.0;
+      out_data[out_idx_b++] = src_b;// / 255.0;//0 * 255.0;
+    }
+  }
+
+  const out  = new onnx.Tensor(out_data, 'float32', [n,c,h,w]);
+
+  return out;
+}
+
+function tensorToContext (tensor, canvasId) {
+  const h = tensor.dims[2];
+  const w = tensor.dims[3];
+  var t_data = tensor.data;
+
+  t_idx_r = 0;
+  t_idx_g = t_idx_r + (h*w);//(h*2*w*2);
+  t_idx_b = t_idx_g + (h*w);//(h*2*w*2);
+
+  var dst_ctx = document.getElementById(canvasId).getContext("2d");
+  var dst_ctx_imgData = dst_ctx.getImageData(0, 0, w, h);
+  var dst_ctx_data = dst_ctx_imgData.data;
+  
+  dst_idx = 0;
+  for (var y=0; y<h; y++) {
+    for (var x=0; x<w; x++) {
+      r = t_data[t_idx_r++];
+      g = t_data[t_idx_g++];
+      b = t_data[t_idx_b++];
+
+      dst_ctx_data[dst_idx++]=r;
+      dst_ctx_data[dst_idx++]=g;
+      dst_ctx_data[dst_idx++]=b;
+      dst_ctx_data[dst_idx++]=0xFF;
+    }
+  }
+  
+  dst_ctx.putImageData(dst_ctx_imgData, 0, 0);
+}
+
+//
+// styleTransfer()
+//     sess - ONNX.js session (from an .onnx)
+//     srcCanvasId - source canvas ID
+//     dstCanvasId - dest canvas ID
+//
+async function styleTransfer(onnxUrl, onnxOutputId, srcCanvasId, dstCanvasId) {
+
+  inputTensor = contextToTensor(srcCanvasId);
+
+  // create and run inference session
+  const sess = new onnx.InferenceSession();
+  await sess.loadModel(onnxUrl);
+  
+  // run inference
+  const t0 = performance.now();
+  const pred = await sess.run([inputTensor]);
+  const t1 = performance.now();
+  
+  // get the result and set it to dest context
+  const output = pred.get(onnxOutputId);
+  tensorToContext (output, dstCanvasId);
+
+  return t1-t0;
+}
+
+
+async function runExample() {
+  setCanvasRGB(canvas_128_dst, 0xFF, 0, 0);
+  setCanvasRGB(canvas_256_dst, 0, 0xFF, 0);
+
+  // load and run inference
+  const modelOuputId = "433";
+
+  // 128x128
+  model128Url = "./onnx_models/candy_128x128.onnx";
+  inferTime_128 = await styleTransfer (model128Url, modelOuputId, canvas_128_src, canvas_128_dst);
+  predictions_128.innerHTML += "<p>inference time: " + inferTime_128 + "ms.</p>";
+
+  // 256x256
+  model256Url = "./onnx_models/candy_256x256.onnx";
+  inferTime_256 = await styleTransfer (model256Url, modelOuputId, canvas_256_src, canvas_256_dst);
+  predictions_256.innerHTML += "<p>inference time: " + inferTime_256 + "ms.</p>";
+  
+}
