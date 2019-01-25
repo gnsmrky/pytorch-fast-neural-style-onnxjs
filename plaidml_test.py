737b8eaead768f5226025a666c80ada8d82c7360
gnsmrky <geniusmark@gmail.com>
2019-01-26 00:22:16 +0800

    add 256x256 zeropad onnx model

--- plaidml_test.py
+++ plaidml_test.py
@@ -3,19 +3,19 @@ import onnx_plaidml.backend
 import numpy as np
 import time
 
-model = onnx.load('onnx_models/candy_128x128_zeropad.onnx')
+model = onnx.load('onnx_models/candy_256x256_zeropad.onnx')
 
-data = np.arange(0.0,3.0*128.0*128.0,1.0).reshape([1,3,128,128])
+data = np.arange(0.0,3.0*256.0*256.0,1.0).reshape([1,3,256,256])
 
 rep = onnx_plaidml.backend.prepare(model)
 
-for i in range (5):
+for i in range (500):
     t0 = time.time()
     #output = onnx_plaidml.backend.run_model(model, [data])
     output = rep.run([data])
     t1 = time.time()
 
-    print(output)
+    #print(output)
     print("inference time:{}".format(t1-t0))
 
 
